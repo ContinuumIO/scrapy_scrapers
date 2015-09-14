7237be4802a71e8239ce1d0b3a47eb12ff01e713
Brittain Hard <brittainhard@gmail.com>
2015-09-14 12:18:11 -0500

    Added pyquery to dependencies.

--- scrapy_scrapers/src/spiders/scrapers.py
+++ scrapy_scrapers/src/spiders/scrapers.py
@@ -7,7 +7,7 @@ class BaseScraper(scrapy.Spider):
     name = "base"
 
     def __init__(self, allowed_domains=[], start_urls=[], *args, **kwargs):
-        # self.allowed_domains = allowed_domains
+        self.allowed_domains = allowed_domains
         self.start_urls = start_urls
         super(BaseScraper, self).__init__(*args, **kwargs)
 
