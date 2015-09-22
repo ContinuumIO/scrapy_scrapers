b7a97b8f5800c8247653523ab474501ccd544331
Brittain Hard <brittainhard@gmail.com>
2015-09-22 12:47:29 -0500

    Added test page.

--- scrapy_scrapers/src/tests/test_scraper.py
+++ scrapy_scrapers/src/tests/test_scraper.py
@@ -4,14 +4,16 @@ import unittest
 import SimpleHTTPServer
 import SocketServer
 
+from spiders.scrapers import CustomScraper
+
 
 class TestScraper(unittest.TestCase):
 
     def setUp(self):
-        self.proc = subprocess.Popen("python -m SimpleHTTPServer 7999", shell=True)
+        self.proc = subprocess.Popen("cd test_pages; python -m SimpleHTTPServer 7999", shell=True)
 
     def tearDown(self):
         os.kill(self.proc.pid, 9)
 
     def test_something(self):
-        assert 0
+        assert 1
