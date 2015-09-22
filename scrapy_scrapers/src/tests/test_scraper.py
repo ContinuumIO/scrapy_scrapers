796bb882ae8565a18c14a7fb426ce632d202ce71
Brittain Hard <brittainhard@gmail.com>
2015-09-22 16:30:44 -0500

    Working test for custom scraper parser.

--- scrapy_scrapers/src/tests/test_scraper.py
+++ scrapy_scrapers/src/tests/test_scraper.py
@@ -3,7 +3,10 @@ import subprocess
 import unittest
 import SimpleHTTPServer
 import SocketServer
+
+import requests
 from elasticsearch import Elasticsearch
+from scrapy.http import Request, TextResponse
 
 from spiders.scrapers import CustomScraper
 
@@ -11,7 +14,21 @@ from spiders.scrapers import CustomScraper
 class TestCustomScraper(unittest.TestCase):
 
     def setUp(self):
-        self.proc = subprocess.Popen("cd test_pages; python -m SimpleHTTPServer 7999", shell=True)
+        self.body = open(os.path.join(os.path.dirname(__file__),
+            "test_pages/dmoz_index.html"), "r").read()
+        self.url = "http://www.example.com/"
+        self.request = Request(self.url)
+        self.response = TextResponse(self.url, request=self.request, body=self.body, encoding='utf-8')
+        self.scraper = CustomScraper(
+            index="test_index",
+            start_urls=[self.url],
+            parser_string="//a",
+            parser_dict={
+                "text": "text()",
+                "link": "@href",
+            }
+        )
 
-    def tearDown(self):
-        os.kill(self.proc.pid, 9)
+    def test_parse(self):
+        parsed = list(self.scraper.parse_item(self.response))
+        assert parsed[1] == {'text': [u'about dmoz'], 'link': [u'http://www.dmoz.org/docs/en/about.html']}
